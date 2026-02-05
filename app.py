9c48c8e72f177329da1681a17c172ab2574b23f7
Prem Repal <84272139+repalPrem11@users.noreply.github.com>
2026-02-05 11:20:36 +0530

    Update app.py
    fixed indantations

--- app.py
+++ app.py
@@ -20,10 +20,12 @@ def mul():
     b = int(request.args.get("b", 0))
     return str(a * b)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
 @app.route("/health")
 def health():
     return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
 
